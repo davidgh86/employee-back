5e0b36cd824358a4389b681021d1358e07fb71b8
David <david_gh86@hotmail.es>
2022-11-01 20:51:54 +0100

    pagination helper

--- src/server/models/adverts.js
+++ src/server/models/adverts.js
@@ -1,50 +1,6 @@
 const { esclient, index } = require("../../elastic/model/advert");
 const { buildElasticSearch, advertHitToAdvert } = require("../../mapper/elasticSearchQueryMapper")
-        require("dotenv").config();
-
-const defaultKeepAlive = parseInt(process.env.KEEP_ALIVE_ES_PAGINATION)
-const defaultPageSize = parseInt(process.env.PAGE_SIZE_ES_PAGINATION)
-
-async function getPaginationId(keepAliveMin) {
-  const pitRes = await esclient.openPointInTime({
-    index: index,
-    keep_alive: keepAliveMin+"m"
-  });
-  return pitRes.id;
-}
-
-async function preparePagination(pagination) {
-  // TODO check que pasa cuando caduca el keepAlive
-  const _pagination = pagination||{}
-
-  if (!_pagination.pitId){
-    _pagination.pitId = await getPaginationId(defaultKeepAlive)
-    _pagination.offset = undefined
-  }
-
-  if (!_pagination.keepAliveMin) {
-    _pagination.keepAliveMin = defaultKeepAlive
-  }
-
-  if (!_pagination.size) {
-    _pagination.size = defaultPageSize
-  }
-
-  return _pagination
-}
-
-function buildPage(results, pitId, offset, total) {
-
-  const _offset = (offset && offset.length>0)?offset:undefined
-  
-  return {
-    id: pitId,
-    offset: _offset,
-    total: total,
-    results: results
-  }
-}
-
+const { preparePagination, buildPage } = require("../../utils/paginationHelper")
 
 async function findAdverts(queryParams, pagination) {
  
